4d64b76287d4b45988de14f36d1431c2c3c91d8f
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-01 21:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var katzDeli = []
-var deliLine = []
 
 function takeANumber(queue, name){
     var position = queue.push(name)
     return `Welcome, ${name}. You are number ${position} in line.`
 }
 
-function nowServing(){
+function nowServing(deliLine){
     var name = deliLine.shift()
     if (name === 'undefined')
         return 'There is nobody waiting to be served!'
