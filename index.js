c81a98248ccf7f50be444824e93788eb2dec6fef
Tim Shaker <tmshkr@users.noreply.github.com>
2018-07-01 21:50:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(queue, name){
     var position = queue.push(name)
     return `Welcome, ${name}. You are number ${position} in line.`
 }
+
+function nowServing(){
+    var name = katzDeli.shift()
+    if (name === undefined)
+        return 'There is nobody waiting to be served!'
+    else return name
+}
